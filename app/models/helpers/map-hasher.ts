2f307e113e5f5eb6db573f5ddc91221281234185
Nick Köllhofer <nk@braun-its.de>
2021-09-09 19:24:20 +0200

    update hasher typo

--- app/models/helpers/map-hasher.ts
+++ app/models/helpers/map-hasher.ts
@@ -1,20 +1,21 @@
-import * as crypto from 'crypto';
-import * as fs from 'fs-extra';
+import { createHash } from 'crypto';
+import { readFileSync } from 'fs';
+import { readFile } from 'fs-extra';
 import { join } from 'path';
 
 export default class MapHasher {
-    public static async Compute(folderPath: string): Promise<string | undefined> {
+    public static async compute(folderPath: string): Promise<string | undefined> {
         try {
             const infoDatPath = join(folderPath, 'info.dat');
-            const infoDatStr = (await fs.readFile(infoDatPath)).toString();
+            const infoDatStr = (await readFile(infoDatPath)).toString();
             const infoDat = JSON.parse(infoDatStr);
             let binary = infoDatStr;
             for (const diffSet of infoDat._difficultyBeatmapSets) {
                 for (const d of diffSet._difficultyBeatmaps) {
-                    binary += fs.readFileSync(join(folderPath, d._beatmapFilename)).toString();
+                    binary += readFileSync(join(folderPath, d._beatmapFilename)).toString();
                 }
             }
-            return crypto.createHash('sha1').update(binary).digest('hex').toLowerCase();
+            return createHash('sha1').update(binary).digest('hex').toLowerCase();
         } catch (e) {
             return undefined;
         }
