2eaf1af63229457b8ce0f3aa94b9fe8d51ae3b93
Nick Köllhofer <nk@braun-its.de>
2021-09-16 11:23:07 +0200

    add missing button module

--- src/app/components/pages/local-maps/local-maps.module.ts
+++ src/app/components/pages/local-maps/local-maps.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 import { SharedAppModule } from '../../../shared/shared-app-module';
 import { LocalMapsComponent } from './local-maps.component';
 import { LocalMapsRounting } from './local-maps.routing';
 
 @NgModule({
     declarations: [LocalMapsComponent],
-    imports: [LocalMapsRounting, CommonModule, SharedAppModule],
+    imports: [LocalMapsRounting, CommonModule, SharedAppModule, ButtonModule],
     exports: [LocalMapsComponent],
     providers: []
 })
