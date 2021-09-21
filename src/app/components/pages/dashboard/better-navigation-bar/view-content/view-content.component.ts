5b95de62e798eae8231497c28b764b11d28dc45d
Nick Köllhofer <nk@braun-its.de>
2021-09-22 01:10:02 +0200

    add mapDetail livetime (unload after 60 sek)

--- src/app/components/pages/dashboard/better-navigation-bar/view-content/view-content.component.ts
+++ src/app/components/pages/dashboard/better-navigation-bar/view-content/view-content.component.ts
@@ -5,8 +5,11 @@ import {
     EmbeddedViewRef,
     Input,
     OnDestroy,
+    OnInit,
     TemplateRef
 } from '@angular/core';
+import { interval, of, Subject } from 'rxjs';
+import { switchMap, take, tap } from 'rxjs/operators';
 import { UnsubscribeComponent } from '../../../../../../models/angular/unsubscribe.model';
 import { ViewTemplateDirective } from '../view-template/view-template.directive';
 
@@ -17,7 +20,7 @@ import { ViewTemplateDirective } from '../view-template/view-template.directive'
 })
 export class ViewContentComponent
     extends UnsubscribeComponent
-    implements AfterContentInit, OnDestroy
+    implements AfterContentInit, OnDestroy, OnInit
 {
     @ContentChild(ViewTemplateDirective) template?: ViewTemplateDirective;
 
@@ -25,16 +28,38 @@ export class ViewContentComponent
 
     view!: EmbeddedViewRef<any> | null;
 
-    public loaded: boolean;
+    private _selectedChange: Subject<boolean>;
+
+    private _loaded: boolean;
+    get loaded(): boolean {
+        return this._loaded;
+    }
+    set loaded(val: boolean) {
+        if (val !== this._loaded) this._loaded = val;
+    }
 
     private _selected: boolean;
     @Input()
     set selected(val: boolean) {
-        this._selected = val;
-        if (val) this.loaded = true;
+        if (this._selected !== val) {
+            this._selected = val;
+            this._selectedChange.next(val);
+            if (val) this.loaded = true;
+        }
     }
     get selected(): boolean {
         return this._selected;
+    }
+
+    private _livetime: number;
+    @Input()
+    get livetime(): number {
+        return this._livetime;
+    }
+    set livetime(val: number) {
+        if (val !== this._livetime) {
+            this._livetime = val;
+        }
     }
 
     @Input()
@@ -42,8 +67,32 @@ export class ViewContentComponent
 
     constructor() {
         super();
-        this.loaded = false;
+        this._loaded = false;
         this._selected = false;
+        this._selectedChange = new Subject<boolean>();
+        this._livetime = 0;
+    }
+
+    ngOnInit(): void {
+        if (this.livetime > 0) {
+            this.addSub(
+                this._selectedChange.pipe(
+                    switchMap((val: boolean) => {
+                        if (val) {
+                            return of(null);
+                        } else if (this.loaded) {
+                            return interval(this.livetime).pipe(take(1));
+                        }
+                        return of(null);
+                    }),
+                    tap((res: number | null) => {
+                        if (res != null) {
+                            this.loaded = false;
+                        }
+                    })
+                )
+            );
+        }
     }
 
     ngAfterContentInit(): void {
